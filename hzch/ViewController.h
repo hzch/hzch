edf848e314ac888cfe2171aa293623dc8704c694
hzch <jch.main@gmail.com>
2016-02-03 10:30:46 +0800

    init project for hzch

---
+++ hzch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hzch
+//
+//  Created by Jiang on 16/2/3.
+//  Copyright © 2016年 hzch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ hzchTests/hzchTests.m
@@ -0,0 +1,39 @@
+//
+//  hzchTests.m
+//  hzchTests
+//
+//  Created by Jiang on 16/2/3.
+//  Copyright © 2016年 hzch. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface hzchTests : XCTestCase
+
+@end
+
+@implementation hzchTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
